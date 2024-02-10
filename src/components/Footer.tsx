fba459434fda7f429cacdebea366470827720d42
naveen8801 <naveen.sharma@stackguardian.io>
2024-02-10 16:48:05 +0530

    refactor text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { IData } from "./../types/index";
 
-const Footer: React.FC<{ data: IData }> = (pops): React.ReactElement => {
+const Footer: React.FC<{ data: IData }> = (props): React.ReactElement => {
+  console.log({ props });
   return <div>Footer</div>;
 };
 
