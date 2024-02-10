65ef50d4b0118d363877cc319a6d67280a6c3271
naveen8801 <naveen.sharma@stackguardian.io>
2024-02-10 16:37:23 +0530

    Added tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>hey !</div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
