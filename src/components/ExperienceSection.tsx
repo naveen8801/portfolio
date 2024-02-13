1dea2bd6a7d992d5dfd1c24139b9c1cc61784073
naveen8801 <naveen.sharma@stackguardian.io>
2024-02-13 21:05:37 +0530

    refactor stylings

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -3,8 +3,13 @@ import React from "react";
 const ExperienceSection: React.FC = (): React.ReactElement => {
   return (
     <div className="w-full box-border h-fit my-16 p-2 md:3 lg:4">
-      <div className="w-full flex flex-col gap-3 text-center justify-center">
-        <h1 className="text-5xl font-bold">Experience</h1>
+      <div className="w-full flex flex-col gap-3 justify-center items-center">
+        <h1 className="w-full text-5xl text-center font-bold md:text-left lg:text-left">
+          Experience
+        </h1>
+        <div className="w-full flex flex-wrap items-center justify-center md:items-start md:justify-start gap-2">
+          Test
+        </div>
       </div>
     </div>
   );
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -3,8 +3,13 @@ import React from "react";
 const ProjectsSection: React.FC = (): React.ReactElement => {
   return (
     <div className="w-full box-border h-fit my-16 p-2 md:3 lg:4">
-      <div className="w-full flex flex-col gap-3 text-center justify-center">
-        <h1 className="text-5xl font-bold">Projects</h1>
+      <div className="w-full flex flex-col gap-3 justify-center items-center">
+        <h1 className="w-full text-5xl text-center font-bold md:text-left lg:text-left">
+          Projects
+        </h1>
+        <div className="w-full flex flex-wrap items-center justify-center md:items-start md:justify-start gap-2">
+          Test
+        </div>
       </div>
     </div>
   );
