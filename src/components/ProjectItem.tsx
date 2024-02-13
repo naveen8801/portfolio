2b84ceb287af7bb7ebf37204cb7c7d825488936e
naveen8801 <naveen.sharma@stackguardian.io>
2024-02-13 22:05:10 +0530

    Added projects section

---
+++ src/components/ProjectItem.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+import { FaGithub, FaTools } from "react-icons/fa";
+
+interface IProps {
+  title: string;
+  description: string;
+  sourceCodeLink: string;
+  technologies: string[];
+}
+
+const ProjectItem: React.FC<IProps> = (props): React.ReactElement => {
+  const { title, description, sourceCodeLink, technologies } = props;
+
+  return (
+    <div className="w-full my-8">
+      <h2 className="text-xl font-semibold">{title}</h2>
+      <h2 className="flex flex-row justify-start gap-1 items-center">
+        <FaGithub className="text-slate-600 dark:text-slate-400" />
+        <Link
+          className="text-blue-500 dark:text-blue-400 "
+          href={sourceCodeLink}
+          target="_blank"
+        >
+          {sourceCodeLink?.split("/")[3] + "/" + sourceCodeLink?.split("/")[4]}
+        </Link>
+      </h2>
+      <div className="flex flex-row justify-start items-center gap-1 text-m text-slate-600 dark:text-slate-400">
+        <FaTools />
+        <span className="text-justify">{technologies?.join(", ")}</span>
+      </div>
+      <ul className="list-disc text-m text-slate-600 dark:text-slate-400">
+        <li className="text-justify">{description}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectItem;
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+"use client";
+import { DataProviderContext } from "@/context";
+import React, { useContext } from "react";
+import ProjectItem from "./ProjectItem";
 
 const ProjectsSection: React.FC = (): React.ReactElement => {
+  const data = useContext(DataProviderContext).Projects;
   return (
     <div className="w-full box-border h-fit my-16 p-2 md:3 lg:4">
       <div className="w-full flex flex-col gap-3 justify-center items-center">
         <h1 className="w-full text-5xl text-center font-bold md:text-left lg:text-left">
           Projects
         </h1>
-        <div className="w-full flex flex-wrap items-center justify-center md:items-start md:justify-start gap-2">
-          Test
+        <div className="w-full">
+          {data.map((item, idx) => (
+            <ProjectItem key={idx} {...item} />
+          ))}
         </div>
       </div>
     </div>
